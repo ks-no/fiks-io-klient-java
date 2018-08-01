8d249f9b989d454f0d42d1181cb908db432a7781
Idar Borlaug <idar.borlaug@gmail.com>
2018-08-01 12:25:44 +0200

    Rename mottak client

--- generateFeignClient.sh
+++ generateFeignClient.sh
@@ -3,6 +3,6 @@ mkdir -p target
 if [ ! -f target/swagger-codegen-cli.jar ]; then
     wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O target/swagger-codegen-cli.jar
 fi
-mkdir -p mottak
-rm -rf mottak/*
-java -jar target/swagger-codegen-cli.jar generate -i ~/.m2/repository/no/ks/fiks/svarinn-mottak-service/1.0.0-SNAPSHOT/svarinn-mottak-service-1.0.0-SNAPSHOT-svarinn-mottak-api-v1.json -l java --library feign -c mottak_config.json -o mottak
+mkdir -p mottak-api-client
+rm -rf mottak-api-client/*
+java -jar target/swagger-codegen-cli.jar generate -i ~/.m2/repository/no/ks/fiks/svarinn-mottak-service/1.0.0-SNAPSHOT/svarinn-mottak-service-1.0.0-SNAPSHOT-svarinn-mottak-api-v1.json -l java --library feign -c mottak_config.json -o mottak-api-client
--- svarinn2.0-klient/src/main/java/no/ks/fiks/amqp/RabbitMqHeaders.java
+++ svarinn2.0-klient/src/main/java/no/ks/fiks/amqp/RabbitMqHeaders.java
@@ -8,9 +8,5 @@ public final class RabbitMqHeaders {
     public static final String AVSENDER_NAVN = "avsender-navn";
     public static final String MELDING_TYPE = "type";
     public static final String SVAR_PA_MELDING_ID = "svar-til";
-
-    public static final String DEAD_LETTER_EXCHANGE = "x-dead-letter-exchange";
-    public static final String DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
-    public static final String DEAD_LETTER_FIRST_DEATH_REASON = "x-first-death-reason";
-    public static final String DEAD_LETTER_FIRST_DEATH_QUEUE = "x-first-death-queue";
+    
 }
