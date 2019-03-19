5fb16940ea39e2d5350051b8cef8ef57c939863e
Sondre Eikanger Kvalø <zapodot@gmail.com>
2019-03-19 09:47:09 +0100

    Add tests

--- src/main/java/no/ks/fiks/svarinn/client/SvarInnHandler.java
+++ src/main/java/no/ks/fiks/svarinn/client/SvarInnHandler.java
@@ -2,16 +2,29 @@ package no.ks.fiks.svarinn.client;
 
 import io.vavr.control.Option;
 import lombok.NonNull;
-import no.ks.fiks.svarinn.client.model.*;
+import lombok.extern.slf4j.Slf4j;
+import no.ks.fiks.svarinn.client.model.KontoId;
+import no.ks.fiks.svarinn.client.model.MeldingRequest;
+import no.ks.fiks.svarinn.client.model.MottattMelding;
+import no.ks.fiks.svarinn.client.model.Payload;
+import no.ks.fiks.svarinn.client.model.SendtMelding;
 import no.ks.fiks.svarinn2.klient.MeldingSpesifikasjonApiModel;
+import no.ks.fiks.svarinn2.klient.SendtMeldingApiModel;
 import no.ks.fiks.svarinn2.klient.SvarInnUtsendingKlient;
 
+import java.io.InputStream;
+import java.security.cert.X509Certificate;
 import java.util.List;
+import java.util.UUID;
 
+@Slf4j
 class SvarInnHandler {
     private KontoId kontoId;
+
     private SvarInnUtsendingKlient utsendingKlient;
+
     private final KatalogHandler katalogHandler;
+
     private final AsicHandler asic;
 
     SvarInnHandler(@NonNull KontoId kontoId,
@@ -26,23 +39,42 @@ class SvarInnHandler {
     }
 
     SendtMelding send(@NonNull MeldingRequest request, @NonNull List<Payload> payload) {
-        return SendtMelding.fromSendResponse(utsendingKlient.send(
-                MeldingSpesifikasjonApiModel.builder()
-                        .avsenderKontoId(kontoId.getUuid())
-                        .mottakerKontoId(request.getMottakerKontoId().getUuid())
-                        .meldingType(request.getMeldingType())
-                        .svarPaMelding(request.getSvarPaMelding() == null ? null : request.getSvarPaMelding().getUuid())
-                        .ttl(request.getTtl().getSeconds())
-                        .build(),
-                payload.isEmpty() ? Option.none() : Option.some(asic.encrypt(katalogHandler.getPublicKey(request.getMottakerKontoId()), payload))));
+        return SendtMelding.fromSendResponse(getSend(request, payload));
     }
 
     SvarSender buildKvitteringSender(@NonNull Runnable acknowledge, @NonNull MottattMelding melding) {
         return SvarSender.builder()
-                .doQueueAck(acknowledge)
-                .utsendingKlient(utsendingKlient)
-                .meldingSomSkalKvitteres(melding)
-                .encrypt(payload -> asic.encrypt(katalogHandler.getPublicKey(melding.getAvsenderKontoId()), payload))
-                .build();
+                         .doQueueAck(acknowledge)
+                         .utsendingKlient(utsendingKlient)
+                         .meldingSomSkalKvitteres(melding)
+                         .encrypt(payload -> encrypt(payload, melding.getAvsenderKontoId()))
+                         .build();
+    }
+
+    private SendtMeldingApiModel getSend(@NonNull final MeldingRequest request, @NonNull final List<Payload> payload) {
+        final UUID mottagerKontoId = request.getMottakerKontoId()
+                                            .getUuid();
+        log.debug("Sender melding til \"{}\"", mottagerKontoId);
+        return utsendingKlient.send(
+            MeldingSpesifikasjonApiModel.builder()
+                                        .avsenderKontoId(kontoId.getUuid())
+                                        .mottakerKontoId(mottagerKontoId)
+                                        .meldingType(request.getMeldingType())
+                                        .svarPaMelding(request.getSvarPaMelding() == null ? null : request.getSvarPaMelding()
+                                                                                                          .getUuid())
+                                        .ttl(request.getTtl()
+                                                    .getSeconds())
+                                        .build(),
+            payload.isEmpty() ? Option.none() : Option.some(encrypt(payload, request.getMottakerKontoId())));
+    }
+
+    private InputStream encrypt(@NonNull final List<Payload> payload, final KontoId kontoId) {
+        log.debug("Krypterer melding til konto \"{}\"", kontoId);
+        return asic.encrypt(getPublicKey(kontoId), payload);
+    }
+
+    private X509Certificate getPublicKey(final KontoId kontoId) {
+        log.debug("Henter offentlig nøkkel for konto \"{}\"", kontoId);
+        return katalogHandler.getPublicKey(kontoId);
     }
 }
