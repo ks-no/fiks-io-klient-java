259c3516ac5d450d61bfffa69d28976ec170468d
jarle børsheim <jarle.borsheim@gmail.com>
2021-06-17 10:48:05 +0200

    Feilmeldinger for ugyldigforespørsel og serverfeil

---
+++ src/main/java/no/ks/fiks/io/client/model/feilmelding/FeilmeldingMeldingTypeV1.java
@@ -0,0 +1,6 @@
+package no.ks.fiks.io.client.model.feilmelding;
+
+public class FeilmeldingMeldingTypeV1 {
+    public static final String Ugyldigforespørsel = "no.ks.fiks.kvittering.ugyldigforespørsel.v1";
+    public static final String Serverfeil = "no.ks.fiks.kvittering.serverfeil.v1";
+}
