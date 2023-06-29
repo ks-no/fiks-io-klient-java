9abf4917a813979c6aaec69cc90c36ea2d668038
helgeskjellevik <helge.skjellevik@gmail.com>
2023-06-29 14:49:26 +0200

    Fix for å sette klientMeldingId vha MeldingRequest-builder

--- src/main/java/no/ks/fiks/io/client/model/MeldingRequest.java
+++ src/main/java/no/ks/fiks/io/client/model/MeldingRequest.java
@@ -6,8 +6,9 @@ import lombok.Value;
 
 import javax.annotation.Nullable;
 import java.time.Duration;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Value
 @Builder
@@ -17,5 +18,40 @@ public class MeldingRequest implements MeldingSpesifikasjon {
     @Nullable private Duration ttl;
     @Nullable private MeldingId svarPaMelding;
     @Nullable private MeldingId klientMeldingId;
-    @Builder.Default private Map<String, String> headere = Collections.emptyMap();
+    @Nullable private Map<String, String> headere;
+
+    // Definerer denne for å hjelpe javadoc. Blir fylt ut av lombok pga Builder-annotasjon.
+    public static class MeldingRequestBuilder {}
+
+    public static MeldingRequestBuilder builder() {
+        return new CustomMeldingRequestBuilder();
+    }
+
+    private static class CustomMeldingRequestBuilder extends MeldingRequestBuilder {
+
+        @Override
+        public MeldingRequest build() {
+            final Map<String, String> copyHeadere;
+            if(null == super.headere) {
+                copyHeadere = new HashMap<>();
+            } else {
+                copyHeadere = new HashMap<>(super.headere);
+            }
+
+            final MeldingId klientMeldingId = super.klientMeldingId;
+            if(null != klientMeldingId) {
+                final String headerKlientMeldingId = copyHeadere.get(Melding.HeaderKlientMeldingId);
+
+                if(null == headerKlientMeldingId) {
+                    copyHeadere.put(Melding.HeaderKlientMeldingId, klientMeldingId.getUuid().toString());
+                } else if(!Objects.equals(klientMeldingId.getUuid().toString(), headerKlientMeldingId)) {
+                    throw new IllegalArgumentException(String.format("Property klientMeldingId er ulik %s-entry angitt via headere-property.", Melding.HeaderKlientMeldingId));
+                }
+            }
+
+            super.headere = copyHeadere;
+
+            return super.build();
+        }
+    }
 }
