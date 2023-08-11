6b1571601bea20c5777df0741e3224ceb349dae0
Sondre Eikanger Kvalø <zapodot@gmail.com>
2023-08-11 14:34:26 +0200

    Endrer default verdier for maskinporten. Deprekerer VER2

--- src/main/java/no/ks/fiks/io/client/konfigurasjon/IdPortenKonfigurasjon.java
+++ src/main/java/no/ks/fiks/io/client/konfigurasjon/IdPortenKonfigurasjon.java
@@ -14,31 +14,47 @@ public class IdPortenKonfigurasjon {
     /**
      * Påkrevd. Uri som skal benyttes for å utføre token-request mot ID-Porten.
      */
-    @NonNull private String accessTokenUri;
+    @NonNull
+    private String accessTokenUri;
 
     /**
      * Påkrevd. Aud-claim for request tokens mot ID-Porten
      */
-    @NonNull private String idPortenAudience;
+    @NonNull
+    private String idPortenAudience;
 
     /**
      * Påkrevd. Organisasjonens oauth klient-id, som registrert hos ID-Porten.
      */
-    @NonNull private String klientId;
+    @NonNull
+    private String klientId;
 
-    public static class IdPortenKonfigurasjonBuilder {};
+    public static class IdPortenKonfigurasjonBuilder {
+    }
+
+    ;
 
     /**
      * Builder med ferdig prod config. Trenger klientId.
      */
     public static final IdPortenKonfigurasjonBuilder PROD = IdPortenKonfigurasjon.builder()
-        .accessTokenUri("https://oidc.difi.no/idporten-oidc-provider/token")
-        .idPortenAudience("https://oidc.difi.no/idporten-oidc-provider/");
+        .accessTokenUri("https://maskinporten.no/token")
+        .idPortenAudience("https://maskinporten.no/");
 
     /**
      * Builder med ferdig ver2 config. Trenger klientId. Brukes mot Fiks IO i test.
+     *
+     * @deprecated Bruk {@link #TEST} i stedet. Digdir holder på å fase ut VER2
      */
+    @Deprecated(since = "3.3.0", forRemoval = true)
     public static final IdPortenKonfigurasjonBuilder VER2 = IdPortenKonfigurasjon.builder()
         .accessTokenUri("https://oidc-ver2.difi.no/idporten-oidc-provider/token")
         .idPortenAudience("https://oidc-ver2.difi.no/idporten-oidc-provider/");
+
+    /**
+     * Builder med ferdig TEST config. Kan brukes mot Fiks IO sitt testmiljø. Trenger klientId.
+     */
+    public static final IdPortenKonfigurasjonBuilder TEST = IdPortenKonfigurasjon.builder()
+        .accessTokenUri("https://test.maskinporten.no/token")
+        .idPortenAudience("https://test.maskinporten.no/");
 }
