5266ed4187ea135da5890df284efd45144da20ee
ErlendStokkeMonstadKS <erlend.stokke.monstad@ks.no>
2026-03-27 13:55:41 +0100

    La til flere tester ihenhold til PR

--- fiks-io-klient-java/src/test/java/no/ks/fiks/io/client/SendtMeldingTest.java
+++ fiks-io-klient-java/src/test/java/no/ks/fiks/io/client/SendtMeldingTest.java
@@ -39,53 +39,73 @@ class SendtMeldingTest {
     }
 
     @Test
+    void fromSendResponseWithKlientMeldingIdAndKorrelasjonIdAndKorrelasjonIdDeprecated() {
+        final MeldingId klientMeldingId = new MeldingId(UUID.randomUUID());
+        final KlientKorrelasjonId klientKorrelasjonsId = new KlientKorrelasjonId((UUID.randomUUID().toString()));
+        assertNotNull(klientKorrelasjonsId.getKlientKorrelasjonId());
+
+        final var headere = ImmutableMap.of(
+            Melding.HeaderKlientMeldingId, klientMeldingId.toString(),
+            Melding.HeaderKlientKorrelasjonIdDeprecated, UUID.randomUUID().toString(),
+            Melding.HeaderKlientKorrelasjonId, klientKorrelasjonsId.getKlientKorrelasjonId()
+        );
+
+        final SendtMeldingApiModel sendtMeldingApiModel = lagSendtMeldingApiModel(headere);
+        final SendtMelding sendtMelding = SendtMelding.fromSendResponse(sendtMeldingApiModel);
+
+        validerSendtMelding(sendtMeldingApiModel, sendtMelding, klientKorrelasjonsId.getKlientKorrelasjonId());
+    }
+
+    @Test
     void fromSendResponseWithKlientMeldingIdAndKorrelasjonId() {
         final MeldingId klientMeldingId = new MeldingId(UUID.randomUUID());
         final KlientKorrelasjonId klientKorrelasjonsId = new KlientKorrelasjonId((UUID.randomUUID().toString()));
         assertNotNull(klientKorrelasjonsId.getKlientKorrelasjonId());
 
-        final SendtMeldingApiModel sendtMeldingApiModel = SendtMeldingApiModel.builder()
-            .meldingId(UUID.randomUUID())
-            .mottakerKontoId(UUID.randomUUID())
-            .meldingType("meldingsprotokoll")
-            .avsenderKontoId(UUID.randomUUID())
-            .svarPaMelding(UUID.randomUUID())
-            .dokumentlagerId(UUID.randomUUID())
-            .ttl(TimeUnit.DAYS.toMillis(5L))
-            .headere(
-                ImmutableMap.of(
-                    Melding.HeaderKlientMeldingId, klientMeldingId.toString(),
-                    Melding.HeaderKlientKorrelasjonIdDeprecated, klientKorrelasjonsId.getKlientKorrelasjonId(),
-                    Melding.HeaderKlientKorrelasjonId, klientKorrelasjonsId.getKlientKorrelasjonId()
-                )
-            )
-            .build();
+        final var headere = ImmutableMap.of(
+            Melding.HeaderKlientMeldingId, klientMeldingId.toString(),
+            Melding.HeaderKlientKorrelasjonId, klientKorrelasjonsId.getKlientKorrelasjonId()
+        );
+
+        final SendtMeldingApiModel sendtMeldingApiModel = lagSendtMeldingApiModel(headere);
         final SendtMelding sendtMelding = SendtMelding.fromSendResponse(sendtMeldingApiModel);
+
+        validerSendtMelding(sendtMeldingApiModel, sendtMelding, klientKorrelasjonsId.getKlientKorrelasjonId());
+    }
+
+    @Test
+    void fromSendResponseWithKlientMeldingIdAndKorrelasjonIdDeprecated() {
+        final MeldingId klientMeldingId = new MeldingId(UUID.randomUUID());
+        final KlientKorrelasjonId klientKorrelasjonsId = new KlientKorrelasjonId((UUID.randomUUID().toString()));
+        assertNotNull(klientKorrelasjonsId.getKlientKorrelasjonId());
+
+        final var headere = ImmutableMap.of(
+            Melding.HeaderKlientMeldingId, klientMeldingId.toString(),
+            Melding.HeaderKlientKorrelasjonIdDeprecated, klientKorrelasjonsId.getKlientKorrelasjonId()
+        );
+
+        final SendtMeldingApiModel sendtMeldingApiModel = lagSendtMeldingApiModel(headere);
+        final SendtMelding sendtMelding = SendtMelding.fromSendResponse(sendtMeldingApiModel);
+
+        validerSendtMelding(sendtMeldingApiModel, sendtMelding, klientKorrelasjonsId.getKlientKorrelasjonId());
+    }
+
+    private static void validerSendtMelding(SendtMeldingApiModel sendtMeldingApiModel, SendtMelding sendtMelding, String klientKorrelasjonId) {
         assertAll(
-            () -> assertEquals(sendtMeldingApiModel.getMeldingId(), sendtMelding.getMeldingId().getUuid()),
-            () -> assertEquals(sendtMeldingApiModel.getMottakerKontoId(), sendtMelding.getMottakerKontoId().getUuid()),
-            () -> assertEquals(sendtMeldingApiModel.getAvsenderKontoId(), sendtMelding.getAvsenderKontoId().getUuid()),
-            () -> assertEquals(sendtMeldingApiModel.getSvarPaMelding(), sendtMelding.getSvarPaMelding().getUuid()),
-            () -> assertEquals(sendtMeldingApiModel.getTtl(), sendtMelding.getTtl().toMillis()),
-            () -> assertEquals(sendtMeldingApiModel.getHeadere().get(Melding.HeaderKlientMeldingId), sendtMelding.getKlientMeldingId().toString()),
-            () -> assertEquals(sendtMeldingApiModel.getHeadere().get(Melding.HeaderKlientKorrelasjonIdDeprecated), sendtMelding.getKlientKorrelasjonId().toString()),
-            () -> assertEquals(sendtMeldingApiModel.getHeadere().get(Melding.HeaderKlientKorrelasjonId), sendtMelding.getKlientKorrelasjonId().toString())
+            () -> assertEquals(sendtMelding.getMeldingId().getUuid(), sendtMeldingApiModel.getMeldingId()),
+            () -> assertEquals(sendtMelding.getMottakerKontoId().getUuid(), sendtMeldingApiModel.getMottakerKontoId()),
+            () -> assertEquals(sendtMelding.getAvsenderKontoId().getUuid(), sendtMeldingApiModel.getAvsenderKontoId()),
+            () -> assertEquals(sendtMelding.getSvarPaMelding().getUuid(), sendtMeldingApiModel.getSvarPaMelding()),
+            () -> assertEquals(sendtMelding.getTtl().toMillis(), sendtMeldingApiModel.getTtl()),
+            () -> assertEquals(sendtMelding.getKlientMeldingId().toString(), sendtMeldingApiModel.getHeadere().get(Melding.HeaderKlientMeldingId)),
+            () -> assertEquals(sendtMelding.getKlientKorrelasjonId().toString(), klientKorrelasjonId)
         );
     }
 
     @Test
     void fromSendResponseWithKlientMeldingIdThatIsNotValidUUID() {
         final String klientMeldingId = "NotUUID";
-        final SendtMeldingApiModel sendtMeldingApiModel = SendtMeldingApiModel.builder()
-            .meldingId(UUID.randomUUID())
-            .mottakerKontoId(UUID.randomUUID())
-            .meldingType("meldingsprotokoll")
-            .avsenderKontoId(UUID.randomUUID())
-            .svarPaMelding(UUID.randomUUID())
-            .dokumentlagerId(UUID.randomUUID())
-            .ttl(TimeUnit.DAYS.toMillis(5L))
-            .headere(ImmutableMap.of(Melding.HeaderKlientMeldingId, klientMeldingId))
-            .build();
+        final SendtMeldingApiModel sendtMeldingApiModel = lagSendtMeldingApiModel(ImmutableMap.of(Melding.HeaderKlientMeldingId, klientMeldingId));
         final SendtMelding sendtMelding = SendtMelding.fromSendResponse(sendtMeldingApiModel);
         assertAll(
             () -> assertEquals(sendtMeldingApiModel.getMeldingId(), sendtMelding.getMeldingId().getUuid()),
@@ -96,5 +116,20 @@ class SendtMeldingTest {
             () -> assertEquals(sendtMeldingApiModel.getHeadere().get(Melding.HeaderKlientMeldingId), sendtMelding.getHeadere().get(Melding.HeaderKlientMeldingId)),
             () -> assertNull(sendtMelding.getKlientMeldingId())
         );
+    }
+
+    private static SendtMeldingApiModel lagSendtMeldingApiModel(ImmutableMap<String, String> headere) {
+        return SendtMeldingApiModel.builder()
+            .meldingId(UUID.randomUUID())
+            .mottakerKontoId(UUID.randomUUID())
+            .meldingType("meldingsprotokoll")
+            .avsenderKontoId(UUID.randomUUID())
+            .svarPaMelding(UUID.randomUUID())
+            .dokumentlagerId(UUID.randomUUID())
+            .ttl(TimeUnit.DAYS.toMillis(5L))
+            .headere(
+                headere
+            )
+            .build();
     }
 }
